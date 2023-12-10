98290fe1f9954aecc64967571bb0386444fc2f4a
Robert Aron <robert.aron.tx@gmail.com>
2023-12-10 16:52:01 -0600

    subdivision grid controls

--- src/content/SoundTypeControls.tsx
+++ src/content/SoundTypeControls.tsx
@@ -11,33 +11,83 @@ export function SoundTypeControls() {
   return (
     <div className="grid">
       {beats.map((subbeats, beatIndex) => {
-        return subbeats.map((beatType, subBeatIndex) => {
-          return (
-            <button
-              style={{
-                gridColumnStart: beatIndex+1,
-                gridRowStart: subBeatIndex+1,
-              }}
-              key={`${beatIndex}-${subBeatIndex}`}
-              onClick={() => {
-                const beatsCopy = [...beats];
-                const beatToUpdate = [...beats[beatIndex]];
-                beatToUpdate[subBeatIndex] = cyle[beatType];
-                beatsCopy[beatIndex] = beatToUpdate;
-                setBeats(beatsCopy);
-              }}
-            >
-              {beatType}
-            </button>
-          );
-        });
+        return [
+          <button
+            style={{
+              gridColumnStart: beatIndex + 2,
+              gridRowStart: 1,
+            }}
+            onClick={() =>
+              setBeats(beats.filter((_, index) => index !== beatIndex))
+            }
+          >
+            -
+          </button>,
+          ...subbeats.map((beatType, subBeatIndex) => {
+            return (
+              <button
+                style={{
+                  gridColumnStart: beatIndex + 2,
+                  gridRowStart: subBeatIndex + 2,
+                }}
+                key={`${beatIndex}-${subBeatIndex}`}
+                onClick={() => {
+                  const beatsCopy = [...beats];
+                  const beatToUpdate = [...beats[beatIndex]];
+                  beatToUpdate[subBeatIndex] = cyle[beatType];
+                  beatsCopy[beatIndex] = beatToUpdate;
+                  setBeats(beatsCopy);
+                }}
+              >
+                {beatType}
+              </button>
+            );
+          }),
+        ];
+      })}
+      {/* remove subdivisions */}
+      {beats[0].map((_, subBeatIndex) => {
+        return (
+          <button
+            style={{ gridColumnStart: 1, gridRowStart: subBeatIndex + 2 }}
+            onClick={() =>
+              setBeats(
+                beats.map((subbeat) =>
+                  subbeat.filter((_, index) => index !== subBeatIndex)
+                )
+              )
+            }
+          >
+            -
+          </button>
+        );
       })}
       <button
+        style={{
+          gridRowStart: beats[0].length + 2,
+          gridColumn: `2 / ${beats.length + 2}`,
+        }}
+        className="bg-orange-400"
         onClick={() => {
           return setBeats(beats.map((oldBeat) => [...oldBeat, "secondary"]));
         }}
       >
-        more
+        +
+      </button>
+      <button
+        onClick={() => {
+          return setBeats([
+            ...beats,
+            new Array(beats[0].length).fill("secondary"),
+          ]);
+        }}
+        className="bg-orange-400"
+        style={{
+          gridColumnStart: beats.length + 2,
+          gridRow: `2 / ${beats[0].length + 2}`,
+        }}
+      >
+        +
       </button>
     </div>
   );
