dafe695e9acfa4f84b9e90e0a22b367512810f35
Robert Aron <robert.aron.tx@gmail.com>
2024-01-03 00:59:56 -0600

    toggle group empty string fix

--- src/content/PercentSpeed.tsx
+++ src/content/PercentSpeed.tsx
@@ -10,6 +10,7 @@ export function PercentSpeed() {
       defaultValue={`${percentSpeed}`}
       aria-label="Beats Per Minute Percentage Modifier"
       onValueChange={(val) => {
+        if (val === "") return;
         const newValue = Number(val);
         if (!isNaN(newValue)) setPercentSpeed(newValue);
       }}
@@ -21,7 +22,7 @@ export function PercentSpeed() {
             key={`${buttonPercent}`}
             value={`${buttonPercent}`}
             aria-label={`${buttonPercent * 100}% Speed`}
-            className="flex grow items-baseline justify-center border-x-[.5px] border-y border-black dark:border-slate-700 px-3 py-1 text-lg first:rounded-l first:border-l last:rounded-r last:border-r hover:bg-amber-500/10 focus:z-10 focus:border-amber-500 focus:shadow-[0_0_0_2px] focus:shadow-amber-500 focus:outline-none data-[state=on]:bg-black data-[state=on]:text-white dark:focus:bg-amber-500/10"
+            className="flex grow items-baseline justify-center border-x-[.5px] border-y border-black px-3 py-1 text-lg first:rounded-l first:border-l last:rounded-r last:border-r hover:bg-amber-500/10 focus:z-10 focus:border-amber-500 focus:shadow-[0_0_0_2px] focus:shadow-amber-500 focus:outline-none data-[state=on]:bg-black data-[state=on]:text-white dark:border-slate-700 dark:focus:bg-amber-500/10"
           >
             <span>{buttonPercent * 100}</span>
             <span className="text-sm">%</span>
