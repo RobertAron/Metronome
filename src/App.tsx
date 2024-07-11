31553500db97a067dde6431ce7fe69b8b4ae7f5b
Robert Aron <robert.aron.tx@gmail.com>
2024-07-11 11:38:17 +0900

    minor SEO tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ function App() {
       <MetronomeContext>
         <div className="flex h-full flex-col justify-between font-sans text-black dark:bg-slate-800 dark:text-white">
           <main className="flex h-full items-center justify-center p-1">
+            <h1 className="sr-only">Metronome App - tempo, time signature, subdivision, practice percentage</h1>
             <div className="flex flex-col gap-4">
               <MetronomeVisual />
               <Bpm />
